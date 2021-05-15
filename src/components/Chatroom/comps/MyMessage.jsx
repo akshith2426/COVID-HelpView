33b4b7057f894d5024370f7b0ef3fb59d218c2ed
Sai Akshith V <akshith292@gmail.com>
2021-05-15 21:19:35 +0530

    changed UI of chatroom

--- src/components/Chatroom/comps/MyMessage.jsx
+++ src/components/Chatroom/comps/MyMessage.jsx
@@ -12,7 +12,7 @@ const MyMessage = ({ message }) => {
 	return (
 		<div
 			className="message"
-			style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}
+			style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#615AE1' }}
 		>
 			{message.text}
 		</div>
