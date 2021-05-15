90e9601d95b4afe75bd88264f6822f362dc49f92
Sai Akshith V <akshith292@gmail.com>
2021-05-15 13:54:29 +0530

    first commit into akshith branch

--- src/components/Chatroom/ChatRoom.jsx
+++ src/components/Chatroom/ChatRoom.jsx
@@ -1,10 +1,18 @@
-import React from "react"
-
-const ChatRoom =() => {
-return(
-    <>
-    <div>This the Chatroom route..make nested elements if necessary in this directory only</div>
-    </>
-)
-} 
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './comps/ChatFeed';
+import LoginForm from './comps/LoginForm';
+import './ChatRoom.css';
+const ChatRoom = () => {
+	if (!localStorage.getItem('username')) return <LoginForm />;
+	return (
+		<ChatEngine
+			height="100vh"
+			projectID="7acb769f-184c-4a15-a288-53fbad5e8c73"
+			userName={localStorage.getItem('username')}
+			userSecret={localStorage.getItem('password')}
+			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+		/>
+	);
+};
 export default ChatRoom;
